eef2c76ca4b6bc873b6bdacf9f172c4ab385dc03
coolshivam542 <coolshivam0542@gmail.com>
2019-02-18 09:51:21 -0800

    Add doctor class.........

---
+++ DHospital/ViewModel/Doctor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DHospital.ViewModel
+{
+    public class Doctor
+    {      
+       public Guid Reg_No { get; set; }
+       public int Doctor_Id {get; set;}
+       public string Doctor_FullName { get; set; }
+       public string UIN { get; set;}
+       public string YOE { get; set; } // year of experiance
+       public DateTime Date_of_Birth { get; set; }
+       public string Gendar { get; set;}
+    }
+}
