20c44233326e372cbee41f7a7e2ab9392f58cfe8
coolshivam542 <coolshivam0542@gmail.com>
2019-02-19 09:24:23 -0800

    Entity for doctor list

---
+++ DHospital/Controllers/DoctorController.cs
@@ -0,0 +1,26 @@
+﻿using DHospital.Models;
+using DHospital.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DHospital.Controllers
+{
+    public class DoctorController : Controller
+    {
+        // GET: Doctor
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult DoctorsList(int id)
+        {
+            Doctor_Context doctor_Context = new Doctor_Context();
+            Doctors doc_list= doctor_Context.Doctors.Single(doc => doc.Id == id);
+            return View();
+        }
+    }
+}
--- DHospital/ViewModel/Doctor.cs
+++ DHospital/ViewModel/Doctor.cs
@@ -1,18 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 
 namespace DHospital.ViewModel
 {
-    public class Doctor
-    {      
-       public Guid Reg_No { get; set; }
-       public int Doctor_Id {get; set;}
-       public string Doctor_FullName { get; set; }
-       public string UIN { get; set;}
-       public string YOE { get; set; } // year of experiance
-       public DateTime Date_of_Birth { get; set; }
-       public string Gendar { get; set;}
+    [Table("Doctor_Details")]
+    public class Doctors
+    {
+      public string Doctor_FullName { get; set;}
+      public string UIN { get; set; }
+      public DateTime YOE { get; set;}
+      public DateTime Date_of_Birth { get; set;}
+      public string Gendar { get; set;}
+        [Key]
+        public int Id { get; set;}
     }
 }
